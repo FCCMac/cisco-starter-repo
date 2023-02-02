a27454432baca89d88ee94748a46679d34754ed4
Chris McAulay <cmcaulay07@gmail.com>
2023-02-01 22:45:59 -0700

    base homepage - navbar and exhibit

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Exhibit from './components/Exhibit';
+import logo from './cisco-logo.svg';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Container>
+        <Navbar bg='light' variant='light' className='px-2'>
+          <Container>
+            <Navbar.Brand href='#home'>
+              <img alt='Cisco Logo' src={logo} width='35' height='35' className='d-inline-block align-top' />
+              {' '}
+              Sextant
+            </Navbar.Brand>
+          </Container>
+        </Navbar>
+        <div className='content my-3'>
+          <Exhibit />
+        </div>
+      </Container>
     </div>
   );
 }
